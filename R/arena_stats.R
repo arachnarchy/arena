078dd7a3ccadd57e9f5bb24a4e04a1576b9cbaa6
“arachnarchy” <daniel.zurek@gmail.com>
2017-11-21 12:27:14 -0800

    Implemented visual angle summary stats and some plotting.

--- R/arena_stats.R
+++ R/arena_stats.R
@@ -3,6 +3,8 @@
 library(dplyr)
 library(ggplot2)
 library(utils)
+library(lme4)
+library(nlme)
 
 #------------------------READ & WRANGLE DATA------------------------------------------------------#
 waves <- read.csv(
@@ -24,36 +26,62 @@ waves <- waves %>%
 
 # make new table with means for each trial (each trial contains different number of waves)
 by_trial <- waves %>% group_by(id, treat)
-waves.by_trial <- by_trial %>% summarize(
-  amplitude = mean(amp),
-  velocity = mean(velocity.male),
+waves_by_trial <- by_trial %>% summarize(
+  amplitude_male = mean(amp),
+  velocity_male = mean(velocity.male),
+  visual_angle = mean(visual.angle),
+  velocity_va = mean(visual.velocity),
   duration = mean(duration),
   n.waves = n()
 )
 
 #------------------------SUMMARY STATS------------------------------------------------------------#
 
-by_treatment <- waves.by_trial %>% group_by(treat)
+by_treatment <- waves_by_trial %>% group_by(treat)
 smry.av <- by_treatment %>% summarize(
-  mean.amplitude = mean(amplitude),
-  mean.velocity = mean(velocity),
+  count <- n(),
+  mean.amplitude_male = mean(amplitude_male),
+  mean.velocity_male = mean(velocity_male),
+  mean.visual_angle = mean(visual_angle),
+  mean.velocity_va = mean(velocity_va),
   mean.n = mean(n.waves),
-  sd.amplitude = sd(amplitude),
-  sd.velocity = sd(velocity),
+  sd.amplitude_male = sd(amplitude_male),
+  sd.velocity_male = sd(velocity_male),
+  sd.visual_angle = sd(visual_angle),
+  sd.velocity_va = sd(velocity_va),
   sd.n = sd(n.waves)
 )
 
-test<- aov(n.waves ~ treat, data = waves.by_trial)
+test <-
+  lme(visual_angle ~ treat, random = ~ 1 | id, data = waves_by_trial)
 summary(test)
+anova(test)
 
 #------------------------PLOT ZONE----------------------------------------------------------------#
 
-## Simple scatterplot of amplitude vs velocity
-plot.av <- ggplot(waves.by_trial, aes(x = velocity, y = amplitude)) +
+## 1: Simple scatterplot of male amplitude vs velocity
+plot_ampVvel <- ggplot(waves_by_trial, aes(x = velocity_male, y = amplitude_male)) +
   geom_point() +
   xlab("velocity (degrees/s)") +
   ylab("amplitude (degrees)") +
   theme_classic()
+
+## 2: Simple scatterplot of visual angle vs va velocity
+plot_visVvel <- ggplot(waves_by_trial, aes(x = visual_angle, y = velocity_va)) +
+  geom_point() +
+  xlab("velocity (degrees/s)") +
+  ylab("visual angle (degrees)") +
+  theme_classic()
+
+## 3: plot visual angle by background categories
+plot_visual_angle <- ggplot(waves_by_trial, aes(x = treat, y = visual_angle)) +
+  geom_point(size = 1) +
+  geom_boxplot(alpha = 0) +
+  xlab("background") +
+  ylab("visual angle (degrees)") +
+  theme_classic()
+
+plot_visual_angle
 
 ## same as heatmap
 # df <- data.frame(waves.by_trial$velocity, waves.by_trial$amplitude)
@@ -74,8 +102,8 @@ plot.av <- ggplot(waves.by_trial, aes(x = velocity, y = amplitude)) +
 # 
 #   rm(df)
 
-## same plot but faceted by background
-plot.av.background <- ggplot(waves.by_trial, aes(x = velocity, y = amplitude)) +
+## plot 1 faceted by background
+plot_ampVvel_background <- ggplot(waves_by_trial, aes(x = velocity_male, y = amplitude_male)) +
   geom_point() +
   xlab("velocity (degrees/s)") +
   ylab("amplitude (degrees)") +
@@ -84,6 +112,20 @@ plot.av.background <- ggplot(waves.by_trial, aes(x = velocity, y = amplitude)) +
   theme(strip.background  = element_blank(),
         panel.border = element_rect(fill = NA),
         axis.line = element_blank())
+
+## plot 2 faceted by background
+plot_visVvel_background <- ggplot(waves_by_trial, aes(x = velocity_va , y = visual_angle)) +
+  geom_point() +
+  xlab("velocity (degrees/s)") +
+  ylab("visual angle (degrees)") +
+  facet_wrap(~ treat) +
+  theme_classic() +
+  theme(strip.background  = element_blank(),
+        panel.border = element_rect(fill = NA),
+        axis.line = element_blank())
+
+plot_ampVvel_background
+plot_visVvel_background
 
 ##
 plot.av.by_strokes <- ggplot(waves, aes(x = velocity.male, y = amplitude.male, color = stroke)) +
